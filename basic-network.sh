a615fb933fb8e455ec8a36b9fbfc61ec1b48cc09
Joe Motacek <jmotacek@bp-3.com>
2017-07-31 12:06:17 -0500

    script debugging11

--- basic-network.sh
+++ basic-network.sh
@@ -219,7 +219,7 @@ docker service create -d --name cli \
   --env CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp \
   --workdir /opt/gopath/src/github.com/hyperledger/fabric/peer \
   --mount type=bind,src=/var/run,dst=/host/var/run \
-  --mount type=bind,src=/home/jmotacek/hyperledger-pi-composer/crypto-config,dst=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto \
+  --mount type=bind,src=/home/jmotacek/hyperledger-pi-composer/crypto-config,dst=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/go \
   --mount type=bind,src=/home/jmotacek/hyperledger-pi-composer/channel-artifacts,dst=/opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts \
   --mount type=bind,src=/home/jmotacek/hyperledger-pi-composer/scripts,dst=/opt/gopath/src/github.com/hyperledger/fabric/peer/scripts \
   --mount type=bind,src=/home/jmotacek/hyperledger-pi-composer/chaincode,dst=/opt/gopath/src/github.com/hyperledger/fabric/examples/chaincode \
