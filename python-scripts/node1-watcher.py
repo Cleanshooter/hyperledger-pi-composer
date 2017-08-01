df56e6dbfb67053a30e43c1d8b27ea2671ac24d8
Joe Motacek <jmotacek@bp-3.com>
2017-08-01 14:55:17 -0500

    adding python log monitors for LEDs6

--- python-scripts/node1-watcher.py
+++ python-scripts/node1-watcher.py
@@ -1,12 +1,12 @@
 import time
 import RPi.GPIO as GPIO
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import PatternMatchingEventHandler
 
 
-class MyHandler(FileSystemEventHandler):
+class MyHandler(PatternMatchingEventHandler):
+    patterns = ["*/peer0org1log.txt"]
     def on_modified(self, event):
-        GPIO.setup(18,GPIO.OUT)
         # print "LED on"
         GPIO.output(18,GPIO.HIGH)
         time.sleep(0.1)
@@ -17,9 +17,10 @@ class MyHandler(FileSystemEventHandler):
 if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
+    GPIO.setup(18,GPIO.OUT)
     event_handler = MyHandler()
     observer = Observer()
-    observer.schedule(event_handler, path='/home/jmotacek/hyperledger-pi-composer/logs/peer0org1log.txt', recursive=False)
+    observer.schedule(event_handler, path='/home/jmotacek/hyperledger-pi-composer/logs/', recursive=False)
     observer.start()
 
     try:
