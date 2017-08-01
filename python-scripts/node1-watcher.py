b017a84e5d53dc221cc14a68284bbabca3530213
Joe Motacek <jmotacek@bp-3.com>
2017-08-01 16:54:45 -0500

    adding python log monitors for LEDs10

--- python-scripts/node1-watcher.py
+++ python-scripts/node1-watcher.py
@@ -1,27 +1,24 @@
 import time
 import RPi.GPIO as GPIO
+import subprocess
+import select
 
-def follow(thefile):
-    thefile.seek(0,2)
-    while True:
-        line = thefile.readline()
-        if not line:
-            time.sleep(0.1)
-            continue
-        yield line
+f = subprocess.Popen(['tail','-F',"/home/jmotacek/hyperledger-pi-composer/logs/peer0org1log.txt"],\
+    stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+p = select.poll()
+p.register(f.stdout)
 
-if __name__ == '__main__':
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(18,GPIO.OUT) # Peer 0 Green
-    GPIO.setup(17,GPIO.OUT) # Peer 0 Amber
-    GPIO.setup(27,GPIO.OUT) # Peer 0 Red
-    GPIO.setup(5,GPIO.OUT) # Peer 1 Green
-    GPIO.setup(6,GPIO.OUT) # Peer 1 Amber
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT) # Peer 0 Green
+GPIO.setup(17,GPIO.OUT) # Peer 0 Amber
+GPIO.setup(27,GPIO.OUT) # Peer 0 Red
+GPIO.setup(5,GPIO.OUT) # Peer 1 Green
+GPIO.setup(6,GPIO.OUT) # Peer 1 Amber
 
-    logfile = open("/home/jmotacek/hyperledger-pi-composer/logs/peer0org1log.txt","r")
-    loglines = follow(logfile)
-    for line in loglines:
+while True:
+    if p.poll(1):
+        line = f.stdout.readline()
         GPIO.output(18,GPIO.HIGH)
         time.sleep(0.01)
         GPIO.output(18,GPIO.LOW)
@@ -31,14 +28,16 @@ if __name__ == '__main__':
         # Flip red on when identified as an anchor peer
         if 'Anchor peers for org Org1MSP are anchor_peers:<host:"peer0.org1.example.com"' in line:
             GPIO.output(27,GPIO.HIGH)
+    time.sleep(1)
+        
 
-    # Catch keyboard exists and kill the lights
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        GPIO.output(18,GPIO.LOW)
-        GPIO.output(17,GPIO.LOW)
-        GPIO.output(27,GPIO.LOW)
-        GPIO.output(5,GPIO.LOW)
-        GPIO.output(6,GPIO.LOW)
+# Catch keyboard exists and kill the lights
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.output(18,GPIO.LOW)
+    GPIO.output(17,GPIO.LOW)
+    GPIO.output(27,GPIO.LOW)
+    GPIO.output(5,GPIO.LOW)
+    GPIO.output(6,GPIO.LOW)
