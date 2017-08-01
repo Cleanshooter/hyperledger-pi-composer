62fcf08c6b884f689c89a1d5ec1cd17917c1f32c
Joe Motacek <jmotacek@bp-3.com>
2017-08-01 15:36:14 -0500

    adding python log monitors for LEDs8

--- python-scripts/node1-watcher.py
+++ python-scripts/node1-watcher.py
@@ -5,19 +5,26 @@ from watchdog.events import PatternMatchingEventHandler
 
 
 class MyHandler(PatternMatchingEventHandler):
-    patterns = ["*/peer0org1log.txt"]
+    patterns = ["*/peer0org1log.txt", "*/peer1org1log.txt"]
     def on_modified(self, event):
-        # print "LED on"
-        GPIO.output(18,GPIO.HIGH)
-        time.sleep(0.1)
-        # print "LED off"
-        GPIO.output(18,GPIO.LOW)
+        if event.src_path.endswith("peer0org1log.txt"):
+            GPIO.output(18,GPIO.HIGH)
+            time.sleep(0.05)
+            GPIO.output(18,GPIO.LOW)
+        if event.src_path.endswith("peer1org1log.txt"):
+            GPIO.output(5,GPIO.HIGH)
+            time.sleep(0.05)
+            GPIO.output(5,GPIO.LOW)
 
 
 if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(18,GPIO.OUT)
+    GPIO.setup(18,GPIO.OUT) # Peer 0 Green
+    GPIO.setup(17,GPIO.OUT) # Peer 0 Amber
+    GPIO.setup(27,GPIO.OUT) # Peer 0 Red
+    GPIO.setup(5,GPIO.OUT) # Peer 1 Green
+    GPIO.setup(6,GPIO.OUT) # Peer 1 Amber
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler, path='/home/jmotacek/hyperledger-pi-composer/logs/', recursive=False)
@@ -27,5 +34,8 @@ if __name__ == "__main__":
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
+        GPIO.output(18,GPIO.LOW)
+        GPIO.output(17,GPIO.LOW)
+        GPIO.output(27,GPIO.LOW)
         observer.stop()
     observer.join()
