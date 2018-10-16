39e4bf4b59744221a639e52f4390223c466dd9ea
Petter <petter@agc-ptu001.cs.uit.no>
2018-10-16 11:47:33 +0200

    Added simple chaincode for storing strings of data

---
+++ chaincode/chaincode_datashare/chaincode_datashare.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+type SimpleAsset struct {
+}
+
+func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) pb.Response {
+
+	// Get the args from the transaction proposal
+	args := stub.GetStringArgs()
+	if len(args) != 2 {
+		return shim.Error("Incorrect arguments. Expecting a key and a value")
+	}
+
+	// Set up any variables or assets here by calling stub.PutState()
+
+	// We store the key and the value on the ledger
+	err := stub.PutState(args[0], []byte(args[1]))
+	if err != nil {
+		return shim.Error(fmt.Sprintf("Failed to create asset: %s", args[0]))
+	}
+	return shim.Success(nil)
+}
+
+func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	// Extract the function and args from the transaction proposal
+	fn, args := stub.GetFunctionAndParameters()
+
+	var result string
+	var err error
+	if fn == "set" {
+		result, err = set(stub, args)
+	} else if fn == "get" {
+		result, err = get(stub, args)
+	} else if fn = "getmultiple" {
+		result, err = getmultiple(stub, args)
+	}
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Return the result as success payload
+	return shim.Success([]byte(result))
+}
+
+// Set stores the asset (both key and value) on the ledger. If the key exists,
+// it will override the value with the new one
+func set(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+	if len(args) != 2 {
+		return "", fmt.Errorf("Incorrect arguments. Expecting a key and a value")
+	}
+
+	err := stub.PutState(args[0], []byte(args[1]))
+	if err != nil {
+		return "", fmt.Errorf("Failed to set asset: %s", args[0])
+	}
+	return args[1], nil
+}
+
+// Get returns the value of the specified asset key
+func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+	if len(args) != 1 {
+		return "", fmt.Errorf("Incorrect arguments. Expecting a key")
+	}
+
+	value, err := stub.GetState(args[0])
+	if err != nil {
+		return "", fmt.Errorf("Failed to get asset: %s with error: %s", args[0], err)
+	}
+	if value == nil {
+		return "", fmt.Errorf("Asset not found: %s", args[0])
+	}
+	return string(value), nil
+}
+// Get returns the value of the specified asset key
+func getmultiple(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+	if len(args) != 1 {
+		return "", fmt.Errorf("Incorrect arguments. Expecting a count")
+	}
+
+	value, err := stub.GetStateByRange("", "")
+	if err != nil {
+		return "", fmt.Errorf("Failed to get asset: %s with error: %s", args[0], err)
+	}
+	if value == nil {
+		return "", fmt.Errorf("Asset not found: %s", args[0])
+	}
+	return string(value), nil
+}
+
+
+func main() {
+	err := shim.Start(new(SimpleAsset))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
